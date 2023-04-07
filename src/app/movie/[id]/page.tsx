7a4fd6188d11822d7b8befa7e1f34a43c8b2f211
bokuzminski <bogdankuzminski@gmail.com>
2023-04-07 16:05:00 +0200

    initial details view

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,11 +1,34 @@
 import { DetailedMovie } from "@/lib/movdbTypes";
+import Image from "next/image";
+import detailedMovieStyle from "./movie.module.css";
 
 export default async function MovieDetailsPage(params: {
   params: { id: number };
 }) {
-  const response = await getIndividualMovieDetails(params.params.id);
+  const movie = await getIndividualMovieDetails(params.params.id);
 
-  return <div>{JSON.stringify(response)}</div>;
+  return (
+    <section className={detailedMovieStyle.wrapper}>
+      <div className={detailedMovieStyle.innerWrapper}>
+        <div className={detailedMovieStyle.contentWrapper}>
+          <div className={detailedMovieStyle.posterContainer}>
+            <Image
+              alt={movie.title}
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              width={300}
+              height={450}
+              className={detailedMovieStyle.posterImage}
+            />
+          </div>
+          <section className={detailedMovieStyle.MovieDetailsContainer}>
+            <h1>{movie.title}</h1>
+            <h3>Synopsis</h3>
+            <p>{movie.overview}</p>
+          </section>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 async function getIndividualMovieDetails(movieId: number) {
