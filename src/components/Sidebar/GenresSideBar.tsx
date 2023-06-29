006279aac7aef773ada66a025d605574bf27a7d8
bokuzminski <bogdankuzminski@gmail.com>
2023-06-29 15:54:39 +0200

    added icons to sidebar

--- src/components/Sidebar/GenresSideBar.tsx
+++ src/components/Sidebar/GenresSideBar.tsx
@@ -11,21 +11,79 @@ export default async function GenresSideBar() {
         <Image src={logo} width={200} height={200} alt="logo" />
         <h1 className="text-white uppercase text-l font-semi-bold px-6">Discover</h1>
         <ul className="list-outside list-none pb-4">
-          <Link href={"/"}>
-            <li className="text-white px-10 font-light my-2">Popular</li>
+          <Link href={"/"} className="flex items-center px-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="white"
+              className="w-4 h-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z"
+              />
+            </svg>
+            <li className="text-white px-2 font-light my-2">Popular</li>
           </Link>
-          <Link href={"/"} passHref>
-            <li className="text-white px-10 font-light my-2">Upcoming</li>
+          <Link href={"/"} className="flex items-center px-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="white"
+              className="w-4 h-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
+              />
+            </svg>
+
+            <li className="text-white px-2 font-light my-2">Upcoming</li>
           </Link>
-          <Link href={"/"}>
-            <li className="text-white px-10 font-light my-2">Top rated</li>
+          <Link href={"/"} className="flex items-center px-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="white"
+              className="w-4 h-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+              />
+            </svg>
+            <li className="text-white px-2 font-light my-2">Top rated</li>
           </Link>
         </ul>
         <h1 className="text-white uppercase text-l font-semi-bold px-5">Genres</h1>
         <ul className="list-outside list-none">
           {genres.map((item) => (
-            <Link href={`/genre/${item.id}`} key={item.id}>
-              <li className="text-white px-10 font-light my-2">{item.name}</li>
+            <Link href={`/genre/${item.id}`} key={item.id} className="flex items-center my-2 px-8">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="white"
+                className="w-4 h-4 "
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              <li className="text-white px-2 font-light">{item.name}</li>
             </Link>
           ))}
         </ul>
